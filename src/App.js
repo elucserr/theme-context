b0bee3f3ae5a21872b80dcc49bf32bb3905b628d
Zapatran <zapatran@users.noreply.github.com>
2020-04-20 13:29:50 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container max-w-screen-sm mx-auto px-4">
+      <div className="flex items-center h-screen">
+        <div className="bg-cyan rounded p-4">Test</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
